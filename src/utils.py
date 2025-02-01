f96d8d3f489e572632f9dca620dfb70689971d06
Jeova BL <jeovab115@gmail.com>
2025-02-01 13:38:57 -0300

    feat: Criado as utils do projeto

---
+++ src/utils.py
@@ -0,0 +1,52 @@
+CATEGORIAS_PRODUTOS = [
+    "Eletrônicos", "Roupas", "Alimentos", "Móveis", "Automotivo", "Brinquedos", "Beleza",
+    "Esportes", "Papelaria", "Jardinagem", "Ferramentas", "Pet Shop", "Informática",
+    "Eletrodomésticos", "Calçados", "Perfumaria", "Bebidas", "Higiene Pessoal", "Construção",
+    "Iluminação", "Relógios", "Bolsas e Acessórios", "Suplementos", "Produtos Naturais",
+    "Joias", "Música e Instrumentos", "Games", "Câmeras e Fotografia", "Casa e Decoração",
+    "Artigos para Festas", "Bicicletas e Acessórios", "Produtos Infantis", "Artigos para Viagem",
+    "Moda Íntima", "Utensílios Domésticos", "Material de Escritório", "Automação Residencial",
+    "Piscinas e Acessórios", "Camping e Aventura", "Segurança Eletrônica", "Equipamentos Médicos",
+    "Produtos Sustentáveis", "Artigos para Gatos", "Artigos para Cachorros", "Peixes e Aquarismo",
+    "Produtos Agrícolas", "Ferragens", "Costura e Artesanato", "Livros", "Revistas e Quadrinhos",
+    "Cursos e Educação", "Suprimentos de Impressão", "Papelaria Criativa", "Colecionáveis",
+    "Equipamentos de Som", "Lembrancinhas Personalizadas", "Ferramentas Manuais",
+    "Acessórios para Celulares", "Produtos para Barbeiros", "Manicure e Pedicure",
+    "Acessórios para Carros", "Equipamentos de Cozinha", "Produtos para Churrasco",
+    "Acessórios para Motocicletas", "Skates e Patins", "Produtos para Marcenaria",
+    "Produtos para Eletricistas", "Maquiagem Profissional", "Cosméticos Orgânicos",
+    "Equipamentos de Academia", "Acessórios de Yoga", "Produtos para Massagem",
+    "Remédios e Medicamentos", "Rações e Petiscos", "Produtos de Limpeza",
+    "Embalagens e Descartáveis", "Produtos para Escritório", "Canecas e Copos Personalizados",
+    "Artigos de Luxo", "Smartwatches", "Dispositivos de Realidade Virtual",
+    "Softwares e Aplicativos", "Componentes Eletrônicos", "Equipamentos de Rede",
+    "Óculos de Sol", "Artigos para Pesca", "Produtos para Surf", "Vestuário Tático",
+    "Artigos de Couro", "Tapetes e Cortinas", "Almofadas e Colchas", "Móveis Planejados",
+    "Produtos para Organização", "Acessórios para Banheiro", "Itens de Cozinha Gourmet",
+    "Molduras e Quadros", "Artigos para Escritório Executivo", "Lâmpadas Inteligentes",
+    "Produtos para Robótica", "Drones e Acessórios"
+]
+
+
+STATUS_PEDIDO = ["Pago", "Pendente", "Cancelado", "Enviado"]
+
+ERROS_NOMES = [
+    "J0ão", "Mar1a", "Pedr0", "An@", "Lu!s", "Jos3", "Fel!pe", "Gabr!el", "Car0l", "V!ctor",  
+    "L4ura", "P@trícia", "Fern@ndo", "Th!ago", "Brun@", "Ric@rdo", "Samu3l", "Rob3rto",  
+    "Camil@", "Edw@rd", "Juli@no", "André@", "M@rco", "Hel3na", "N@thalia", "Edu@rdo",  
+    "S0fia", "Be@triz", "Lor3nzo", "R@fael", "Est3ban", "Migu3l", "T@lita", "C@ssia",  
+    "Is@ac", "D@niele", "Henr!que", "V@nessa", "Chri$tian", "Josué@", "F@bio", "Eliz@",  
+    "K@ren", "P@ulo", "C!cero", "Ev3lyn", "D@phne", "Leon@rdo", "R0gério", "Emm@nuel"  
+]
+
+ERROS_EMAIL = [
+    "email.com", "user@", "invalido@site", "exemplo@@gmail.com", "contato@.com",  
+    "@gmail.com", "usuario@gmail..com", "nome.sobrenome@", "testeemail@",  
+    "123@456", "semarroba.com", "email@@outlook.com", "user@@yahoo.com",  
+    "exemplo.com@", "nome@dominio..com", "email sem arroba", "site@dominio@com",  
+    "teste@.com.br", "user@invalido,com", "admin@@hotmail.com", "pessoa@.net",  
+    "info@site..org", "meuemail@errado", "email@err@do.com", "hello@@provider.com",  
+    "example@com@", "usuário@site.123", "email@email,email", "teste@servidor_com",  
+    "conta@email@com", "nome@@servidor.com", "exemplo@err@do.com",  
+    "mail@dominio_com", "abc@xyz..com", "usuario@@site.com", "teste@errado@dominio.com"  
+]
